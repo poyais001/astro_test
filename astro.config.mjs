358b44fcc19b27b69ab7d8c23cd3cbb66f602ef3
Zhao YN - Matsui Lab <zhao-yingnan-df@ynu.jp>
2026-03-16 00:20:29 +0900

    add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://visionary-rolypoly-445885.netlify.app"
+  site: "https://visionary-rolypoly-445885.netlify.app",
+  integrations: [preact()]
 });
